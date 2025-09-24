2c48bfdfafba3369ef6788545245ec9267200c66
Fabián Rodas <fabianrodas2005@gmail.com>
2025-09-23 19:14:54 -0500

    Tabla parte 1 dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { ContactList } from './components/contact-list/contact-list';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ContactList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
