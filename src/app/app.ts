adcb1c6803131b39c66d29097f1eea6bc8c179b3
Fabián Rodas <fabianrodas2005@gmail.com>
2025-09-24 23:58:38 -0500

    Arreglando dashboard + 3 componentes nuevos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { ContactList } from './components/contact-list/contact-list';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [ContactList],
+  imports: [Header, ContactList, Dashboard, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
